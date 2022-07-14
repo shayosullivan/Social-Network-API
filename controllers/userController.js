af8c92083d001c7985d7a300cbfb9d6e5a7b6ed8
shay <shayosullivan@hotmail.com>
2022-07-14 16:04:57 -0400

    update users, in usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,12 +14,26 @@ const userController = {
                 res.json(data);
             }
             else {
-                res.status(404).json({ message: "No user with the id" });
+                res.status(404).json({ message: "No user found with the id" });
             }
         })
         .catch((err) => res.status(500).json(err));
     },
-    
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            req.body
+        )
+        .then((data) => {
+            if(data) {
+                res.json(data);
+            }
+            else {
+                res.status(404).json({ message: "No user found with the id" });
+            }
+        })
+        .catch((err) => res.status(500).json(err));
+    },
 }
 
 module.exports = userController;
