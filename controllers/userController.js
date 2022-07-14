5cce2d34124c91946d8355bf7c7bc39b68fc991d
shay <shayosullivan@hotmail.com>
2022-07-14 16:05:44 -0400

    remove users, in usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,6 +34,22 @@ const userController = {
         })
         .catch((err) => res.status(500).json(err));
     },
+    removeUser(req, res) {
+        User.findOneAndDelete(
+            { _id: req.params.userId }
+        )
+        .then((data) => {
+            if(data) {
+                return Thought.deleteMany(
+                    { username : data.username }
+                );
+            }
+            else {
+                res.status(404).json({ message: "No user found with the id" });
+            }
+        })
+        .catch((err) => res.status(500).json(err));
+    },
 }
 
 module.exports = userController;
